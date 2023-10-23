fe7a82774644bdf79867f709a95911be2291749c
Tibrizzy2 <148764988+Tibrizzy2@users.noreply.github.com>
2023-10-23 12:35:34 +0200

    Add files via upload

---
+++ booking.js
@@ -0,0 +1,10 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyBVhgY0-8FkvGgvONeTu27JcnaDoZwYBss",
+    authDomain: "makhado-689d2.firebaseapp.com",
+    databaseURL: "https://makhado-689d2-default-rtdb.firebaseio.com",
+    projectId: "makhado-689d2",
+    storageBucket: "makhado-689d2.appspot.com",
+    messagingSenderId: "169425355200",
+    appId: "1:169425355200:web:8adb54b955dc02240f0825"
+};
+
